ac08925f8c22b8e8d21ff1e3b00ccbbc2bf28227
Fredrik Lanker <fredrik.lanker@gmail.com>
2017-06-07 21:57:55 +0200

    Archive tree
    Using <localleader>at will move current tree to a file with '_archive'
    appended to its name.

---
+++ ftplugin/orgmode/plugins/Archive.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import vim
+
+import os
+
+from orgmode._vim import echo, ORGMODE, apply_count, repeat, realign_tags
+from orgmode.menu import Submenu, ActionEntry
+from orgmode.keybinding import Keybinding, Plug
+from orgmode.py3compat.py_py3_string import VIM_PY_CALL
+
+
+class Archive(object):
+
+    def __init__(self):
+        u""" Initialize plugin """
+        object.__init__(self)
+        # menu entries this plugin should create
+        self.menu = ORGMODE.orgmenu + Submenu(u'&Archive')
+
+        # key bindings for this plugin
+        # key bindings are also registered through the menu so only additional
+        # bindings should be put in this variable
+        self.keybindings = []
+
+    @classmethod
+    @realign_tags
+    @repeat
+    @apply_count
+    def archive_tree(cls):
+        d = ORGMODE.get_document(allow_dirty=True)
+
+        heading = d.current_heading()
+        if not heading:
+            return
+
+        fn = os.path.splitext(vim.current.buffer.name)
+        filename = fn[0] + '_archive' + fn[1]
+        f = open(filename, "a")
+        if heading.body:
+            f.write("%s\n%s\n" % (heading, "\n".join(heading.body)))
+        else:
+            f.write("%s\n" % (heading))
+        f.close()
+
+        l = heading.get_parent_list()
+        l.remove(heading)
+        d.write()
+
+        echo(u'moved subtree to: %s' % filename)
+        return True
+
+    def register(self):
+        self.keybindings.append(Keybinding(u'<localleader>at', Plug(
+            u'OrgArchiveTreeNonInteractive',
+            u'%s ORGMODE.plugins[u"Archive"].archive_tree()<CR>' % VIM_PY_CALL)))
+        self.menu + ActionEntry(u'&Archive Subtree', self.keybindings[-1])
